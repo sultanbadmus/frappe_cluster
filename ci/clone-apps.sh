be68973d0b7bbdc6c6866da70516bd4a6ebd1494
sultanbadmus <s.badmus@parallelscore.com>
2022-11-30 00:16:04 +0100

    update

--- ci/clone-apps.sh
+++ ci/clone-apps.sh
@@ -21,4 +21,4 @@
 git clone --depth 1 --branch develop https://github.com/frappe/health repos/healthcare
 git clone --depth 1 --branch main https://github.com/frappe/chat repos/chat
 # git clone --depth 1 --branch dev ssh://git@github.com/parallelscore/Lafia-WebApp-ERPNext- repos/lafia
-git clone --depth 1 --branch dev https://sultanbadmus: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@github.com/parallelscore/Lafia-WebApp-ERPNext- repos/lafia
+git clone --depth 1 --branch sultan-patch https://sultanbadmus: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@github.com/parallelscore/Lafia-WebApp-ERPNext- repos/lafia
